2865531c0df414ed481dca423ee611360362624f
nglgzz <nglgzz97@gmail.com>
2017-03-13 00:06:46 +0000

    updated init script: added some checks, switched to symlinks, and removed all relative paths

--- init.sh
+++ init.sh
@@ -1,96 +1,112 @@
 #!/bin/bash
 
-# check if super user, otherwise run sudo
-if [ $EUID != 0 ]; then
-	echo '#  Superuser permission is required.'
-    sudo "$0" "$@"
-    exit $?
+# Check if superuser, otherwise run sudo and pass HOME path and username.
+if [ "$(whoami)" != "root" ]; then
+  echo '#  Superuser permission is required.'
+  sudo "$0" "$@" -- "$HOME" "$(whoami)"
+  exit $?
 fi
 
-echo '#  Installing vim zip i3 i3blocks i3lock xinit and terminator.'
-apt-get install sudo git vim zip i3 i3blocks i3lock xinit terminator --force-yes -qq
+# use the passed HOME path or use the environment variable if
+# not specifies (ie. the script is run as root), same with username
+home=${2:-"$HOME"}
+user=${3:-"$(whoami)"}
+dots=$(dirname $(realpath $0))
 
-
-echo '#  Adding non-free repositories and adding jessie-backports.'
-
-# remove non-free from repositories that contain non-free already
+# Add sources
+echo '#  Adding non-free sources and jessie-backports.'
+# remove non-free if present and then
+# append non-free to all lines starting with 'deb '.
 sed -r 's/^(deb .*)non-free *$/\1/' -i /etc/apt/sources.list
-# add non-free to all repositories (all lines starting with 'deb ')
 sed -r 's/^deb .*/& non-free/' -i /etc/apt/sources.list
 
-# removes backports if present
-sed -r 's/deb http:\/\/debian.jpleau.ca\/ jessie-backports main contrib non-free//' -i /etc/apt/sources.list
-# add backports it
-echo 'deb http://debian.jpleau.ca/ jessie-backports main contrib non-free' >> /etc/apt/sources.list
+# removes backports if present and then append it to sources.list file-
+sed -r 's/deb http:\/\/debian.jpleau.ca\/ jessie-backports main contrib non-free//'\
+ -i /etc/apt/sources.list
+echo 'deb http://debian.jpleau.ca/ jessie-backports main contrib non-free'\
+ >> /etc/apt/sources.list
 
-echo '#  Refreshing packages.'
+echo '#  Updating packages list.'
 apt-get update -qq
 
-echo '#  Installing network manager.'
-apt-get install wicd-gtk -qq
 
-echo '#  Installing browser, volume controls and vlc.'
-apt-get install chromium alsa-utils pavucontrol vlc --force-yes -qq
+# Install packages. May take long.
+essential=(sudo zip xinit i3 i3blocks i3lock build-essential autoconf automake \
+pkg-config libpango1.0-dev libpangocairo-1.0-0 libcairo2-dev libglib2.0-dev \
+libxcb-xkb-dev libxcb-xinerama0-dev libxcb1-dev xutils-dev libtool libxcb-util0-dev \
+libxcb-ewmh-dev libxcb-icccm4-dev libxcb-randr0-dev libstartup-notification0-dev \
+libxkbcommon-dev libxkbcommon-x11-dev bison flex byacc)
 
-echo '#  Installing file manager.'
-apt-get install thunar -qq
+recommended=(git vim alsa-utils scrot feh software-properties-common)
+additional=(chromium pavucontrol vlc nautilus terminator wicd-gtk lxappearance)
 
-echo '#  Downloading Roboto_Mono font.'
-wget -q https://fonts.google.com/download?family=Roboto%20Mono -O Roboto_Mono.zip
-echo '#  Installing Roboto_Mono font.'
-unzip Roboto_Mono.zip -d roboto-mono
-mkdir /usr/share/fonts/truetype
-mv roboto-mono /usr/share/fonts/truetype/
-fc-cache -f
-rm Roboto_Mono.zip
+echo '#  Installing essential packages (may take long).'
+apt-get install ${essential[@]}
 
-echo '#  Downloading Font Awesome.'
+echo '#  Installing recommended packages (may take long).'
+apt-get install ${recommended[@]}
+
+echo '#  Installing additional packages (may take long).'
+apt-get install ${additional[@]}
+
+unset essential
+unset recommended
+unset additional
+
+
+# Install fonts
+echo '#  Downloading Roboto Mono font.'
+wget -q https://fonts.google.com/download?family=Roboto%20Mono -O roboto-mono.zip
+unzip roboto-mono.zip -d roboto-mono
+
+echo '#  Downloading Raleway font.'
+wget -q https://fonts.google.com/download?family=Raleway -O raleway.zip
+unzip raleway.zip -d raleway
+
+echo '#  Downloading Font Awesome font.'
 wget -q http://fontawesome.io/assets/font-awesome-4.7.0.zip -O fontawesome.zip
-echo '#  Installing Font Awesome.'
 unzip fontawesome.zip -d fontawesome
-mkdir .fonts
-mv fontawesome/font-awesome-4.7.0/fonts/* .fonts/
+
+echo '#  Installing fonts.'
+mkdir -p /usr/share/fonts/truetype
+mkdir -p $home/.fonts/
+chown -R $user $home/.fonts/
+
+mv roboto-mono raleway /usr/share/fonts/truetype/
+mv fontawesome/font-awesome-4.7.0/fonts/* $home/.fonts/
 fc-cache -f
-rm fontawesome.zip
-rm -r fontawesome
 
-echo '#  Installing build essentials for building programs.'
-apt-get install build-essential autoconf automake pkg-config --force-yes -qq
+echo '#  Cleaning up.'
+rm -r roboto-mono.zip raleway.zip fontawesome.zip fontawesome
 
+
+# Install text editor
 echo '#  Downloading and installing sublime text 3.'
 wget -q https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O sublime_text3.deb
 dpkg -i sublime_text3.deb
 rm sublime_text3.deb
 
-echo '#  Installing Rofi dependencies.'
-apt-get install libpango1.0-dev libpangocairo-1.0-0 libcairo2-dev libglib2.0-dev libxcb-xkb-dev libxcb-xinerama0-dev -qq
-apt-get install libxcb1-dev xutils-dev libtool libxcb-util0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-randr0-dev -qq
-apt-get install libstartup-notification0-dev -qq
-apt-get install libxkbcommon-dev libxkbcommon-x11-dev --force-yes -qq
-apt-get install bison flex byacc
 
-echo '#  Installing printscreen, wallpaper application and theme customization tool.'
-apt-get install scrot feh toilet lxappearance software-properties-common --force-yes -qq
+# Install i3 dependency
+echo '# Cloning xcb-util-xrm repository from https://github.com/Airblader/xcb-util-xrm.'
+git clone --recursive https://github.com/Airblader/xcb-util-xrm.git $home/xcb-util-xrm --quiet
 
-
-echo '#  Cloning xcb-util-xrm repository from https://github.com/Airblader/xcb-util-xrm.'
-git clone --recursive https://github.com/Airblader/xcb-util-xrm.git --quiet
-
-echo '#  Installing xcb-util-xrm.'
-cd xcb-util-xrm
+echo '# Installing xcb-util-xrm.'
+cd $home/xcb-util-xrm
 autoreconf -i
 mkdir build
-cd build
 ../configure
 make
 make install
-cd ../..
+chown -R $user $home/xcb-util-xrm/
 
+
+# Install Rofi
 echo '#  Cloning Rofi repository from https://github.com/DaveDavenport/rofi.'
-git clone https://github.com/DaveDavenport/rofi.git --quiet
+git clone https://github.com/DaveDavenport/rofi.git $home/rofi --quiet
 
 echo '#  Installing Rofi.'
-cd rofi
+cd $home/rofi
 git submodule update --init
 autoreconf -i
 mkdir build
@@ -98,57 +114,63 @@ cd build
 ../configure
 make
 make install
-cd ../..
 
 echo '#  Adding LIBDIR and LD_LIBRARY_PATH export in .bashrc'
-echo "export LIBDIR='/usr/local/lib'" >> .bashrc
+echo "export LIBDIR='/usr/local/lib:$LIBDIR'" >> .bashrc
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBDIR" >> .bashrc
+
 echo '#  Reload .bashrc'
-source .bashrc
+source $home/.bashrc
 
-echo '#  Styling i3, i3blocks and Rofi.'
-mkdir .i3/
-cp ./dots/i3/* .i3/
 
-echo '#  Styling terminator.'
-mkdir -p .config/terminator
-cp ./dots/terminator.config .config/terminator/config
+# if there's an existing i3 configuration, back it up and delete the folder
+if [ -d "$home/.i3" ]; then
+  echo '#  Backing up  previous i3 configuration to $HOME/i3.back.zip'
+  zip $home/i3.back.zip $home/.i3 -r
+  rm -r $home/.i3
+fi
 
-# https://github.com/horst3180/arc-theme
-echo '#  Installing arc-theme GTK theme.'
-wget http://download.opensuse.org/repositories/home:Horst3180/Debian_8.0/Release.key
-apt-key add - < Release.key
-rm Release.key
-echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/Debian_8.0/ /' > /etc/apt/sources.list.d/arc-theme.list
-apt-get update -qq
-apt-get install arc-theme --force-yes -qq
+echo '#  Linking i3 dots.'
+ln -s  $dots/i3 $home/.i3
+
+# if there's an existing terminator config, back it up.
+if [ -d "$home/.config/terminator/config" ]; then
+  echo '#  Backing up  previous terminator config to $HOME/.config/terminator/config.back'
+  mv $home/.config/terminator/config $home/.config/terminator/config.back
+fi
+
+echo '# Linking terminator config.'
+mkdir -p $home/.config/terminator
+ln -s  $dots/terminator/config $home/.config/terminator/config
 
 # https://snwh.org/paper/download
 echo '#  Installing Paper icons.'
-wget -q 'https://snwh.org/paper/download.php?owner=snwh&ppa=pulp&pkg=paper-icon-theme,16.04' -O paper-icon.deb
+wget -q 'https://snwh.org/paper/download.php?owner=snwh&ppa=pulp&pkg=paper-icon-theme,16.04'\
+ -O paper-icon.deb
+wget -q 'https://snwh.org/paper/download.php?owner=snwh&ppa=pulp&pkg=paper-gtk-theme,16.04'\
+ -O paper-gtk.deb
 dpkg -i paper-icon.deb
+dpkg -i paper-gtk.deb
 apt-get install -f --force-yes -qq
 rm paper-icon.deb
+rm paper-gtk.deb
 
-# Used for Media Keys (doesn't work for me)
-https://github.com/acrisci/playerctl
-echo '#  Installing Playerctl dependencies'
-apt-get install gtk-doc-tools gobject-introspection
-
-echo '#  Installing Playerctl'
-git clone https://github.com/acrisci/playerctl
-cd playerctl
-./autogen.sh
-make
-make install
 
 echo '#  Done, here is a couple of things you may want to do now:'
 echo '#  - select arc-theme on lxappearance'
 echo '#  - select paper icons on lxappearance'
 echo '#  - install following packages for Sublime Text:'
-echo '#  		-Package Control'
-echo '#  		-Material Theme (blue accent)'
-echo '#  		-Emmet'
-echo '#  		-JSX'
+echo '#     -Package Control'
+echo '#     -Material Theme (blue accent)'
+echo '#     -Emmet'
+echo '#     -JSX'
 echo '#  - install firmware-iwlwifi if on laptop'
 echo '#  - run `adduser [username] sudo` to add user to sudoers.'
+unset home user dots
+# Todo:
+#  - add sublime and GTK config linking
+#  - test and update end message
+#  - set up projects folder
+#  - remove update.sh
+#  - fix keypub
+#  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys <PUBKEY>
