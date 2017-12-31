7211d658e3e09ff6e6f1f4743c27aecf709944cf
Angelo Gazzola <angelo@nglgzz.com>
2017-12-31 23:25:32 +0100

    use git submodules, update link script, and update color scheme

--- link.sh
+++ link.sh
@@ -18,25 +18,15 @@ _link() {
   ln -s "$1" "$2"
 }
 
-# Clone a repo from Github
-# _clone user/repo [path]
-_clone() {
-  if [[ ! -d $2 ]]; then
-    git clone https://github.com/$1.git $2 --recursive
-  fi
-}
-
 # Path variables
-dots=~/projects/.dots/config
+dots=~/dots/config
 config=~/.config
-vim_bundle=~/.vim/bundle
-vim_colors=~/.vim/colors
 ignore=(vim sublime-text-3)
 
-_link ~/projects/.gitconfig ~/.gitconfig
-_link ~/projects/.ssh ~/.ssh
-_link ~/projects/.zshrc ~/.zshrc
-cp $dots/albert/albert.conf $config/albert.conf
+#_link ~/projects/.gitconfig ~/.gitconfig
+#_link ~/projects/.ssh ~/.ssh
+#_link ~/projects/.zshrc ~/.zshrc
+#cp $dots/albert/albert.conf $config/albert.conf
 
 # Walk dirs in dots/config folder, and link them
 # in ~/.config folder.
@@ -50,21 +40,7 @@ done
 
 # .vimrc and vim plugins
 _link $dots/vim/vimrc ~/.vimrc
-_link $dots/vim/autoload ~/.vim/autoload
-
-mkdir -p $vim_bundle
-_clone jiangmiao/auto-pairs $vim_bundle/auto-pairs
-_clone pangloss/vim-javascript $vim_bundle/vim-javascript
-_clone valloric/youcompleteme $vim_bundle/youcompleteme
-cd $vim_bundle/youcompleteme
-./install.py --all
-
-mkdir -p $vim_colors
-cd $vim_colors
-
-if [[ ! -f PaperColor.vim ]]; then
-  wget https://raw.githubusercontent.com/nlknguyen/papercolor-theme/master/colors/PaperColor.vim
-fi
+_link $dots/vim ~/.vim
 
 # Sublime
 _link "$dots/sublime-text-3/Package Control.sublime-settings"\
