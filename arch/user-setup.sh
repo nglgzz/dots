8f0d58ccc54d36c2a0121bd38af67c5d482ceb9b
Angelo Gazzola <angelo@nglgzz.com>
2018-01-03 19:45:58 +0100

    install sublime from recommended repository

--- arch/user-setup.sh
+++ arch/user-setup.sh
@@ -31,6 +31,18 @@ rm -r ~/tmp/pacaur_install
 # Install packages
 cat ~/packages.list | sed 's/#.*//' | xargs pacaur -S --noconfirm
 
+
+# Install sublime-text from author repository
+# https://www.sublimetext.com/docs/3/linux_repositories.html#pacman
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && \
+  sudo pacman-key --add sublimehq-pub.gpg && \
+  sudo pacman-key --lsign-key 8A8F901A && \
+  rm sublimehq-pub.gpg
+
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | \
+  sudo tee -a /etc/pacman.conf
+
+
 # Install pip dependencies for Albert
 sudo pip install lxml clipboard requests
 
