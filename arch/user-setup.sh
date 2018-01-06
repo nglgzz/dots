f861854bdd2299a3a26a2377d91fd41b1e22757a
Angelo Gazzola <angelo@nglgzz.com>
2018-01-06 02:16:12 +0100

    forgot to install sublime

--- arch/user-setup.sh
+++ arch/user-setup.sh
@@ -41,6 +41,7 @@ curl -O https://download.sublimetext.com/sublimehq-pub.gpg && \
 
 echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | \
   sudo tee -a /etc/pacman.conf
+pacaur -Syu sublime-text
 
 
 # Install pip dependencies for Albert
