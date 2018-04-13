f581e9eaed2334126ad83eb8e819e285027e79d6
Angelo Gazzola <angelo@nglgzz.com>
2018-04-13 22:06:26 +0200

    updated polybar, added new styles on stylus

--- dots/.config/polybar/launch.sh
+++ dots/.config/polybar/launch.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+SCREEN_WIDTH=$(xrandr | grep " connected" | cut -d ' ' -f3 | sed 's/x.*//' | head -n1)
+
+export MONITOR=$(xrandr | grep " connected" | cut -d ' ' -f1 | head -n1)
+export POLYBAR_WIDTH=$(expr $SCREEN_WIDTH - 80)
+export NETWORK_IFACE=$(ifconfig -s | awk '{ print $1, $3}' | grep -vP '0$|^lo|^Iface' | awk '{ print $1 }')
+
 # Terminate already running bar instances
 killall -q polybar
 
