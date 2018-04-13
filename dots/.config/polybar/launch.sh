f3121b8a12326cfc2f1e31c5becc80bd870c9294
Angelo Gazzola <angelo@nglgzz.com>
2018-04-13 21:39:28 +0200

    fixed polybar width on laptop

--- dots/.config/polybar/launch.sh
+++ dots/.config/polybar/launch.sh
@@ -3,7 +3,7 @@
 SCREEN_WIDTH=$(xrandr | grep " connected" | cut -d ' ' -f3 | sed 's/x.*//' | head -n1)
 
 export MONITOR=$(xrandr | grep " connected" | cut -d ' ' -f1 | head -n1)
-export POLYBAR_WIDTH=$(expr $SCREEN_WIDTH - 80)
+export POLYBAR_WIDTH=$(expr $SCREEN_WIDTH - 80 || expr 1366 - 80)
 export NETWORK_IFACE=$(ifconfig -s | awk '{ print $1, $3}' | grep -vP '0$|^lo|^Iface' | awk '{ print $1 }')
 
 # Terminate already running bar instances
