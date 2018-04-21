1b34f9b8c7032d155d0d6fa72537e81c083eb6f8
Angelo Gazzola <angelo@nglgzz.com>
2018-04-21 11:11:56 +0200

    changed color scheme on polybar, cleaned config, and updated launch script

--- dots/.config/polybar/launch.sh
+++ dots/.config/polybar/launch.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
-
+# Environment variables
 SCREEN_WIDTH=$(xrandr | grep " connected" | cut -d ' ' -f3 | sed 's/x.*//' | head -n1)
 
+## Bar settings
 export MONITOR=$(xrandr | grep " connected" | cut -d ' ' -f1 | head -n1)
 export POLYBAR_WIDTH=$(expr $SCREEN_WIDTH - 80 || expr 1366 - 80)
 export NETWORK_IFACE=$(ifconfig -s | awk '{ print $1, $3}' | grep -vP '0$|^lo|^Iface' | awk '{ print $1 }')
+
 
 # Terminate already running bar instances
 killall -q polybar
@@ -13,4 +15,4 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar default &
+polybar $1 &
