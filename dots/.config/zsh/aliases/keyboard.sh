78a8a6753bd4ef17bcd963a3a72e9617ceae6978
Angelo Gazzola <angelo@nglgzz.com>
2020-07-06 13:11:06 +0200

    Add aliases to disable laptop keyboard

--- dots/.config/zsh/aliases/keyboard.sh
+++ dots/.config/zsh/aliases/keyboard.sh
@@ -6,7 +6,27 @@ declare -A keyboard=(
   [42source]='cd $KEYBOARD_HOME/firmware && make'
   [42edit]='nvim $KEYBOARD_HOME/firmware/42/keymaps/default/keymap.c'
   ['xev-clean']='xev | awk -F'\''[ )]+'\'' '\''/^KeyPress/ { a[NR+2] } NR in a { printf "%-3s %s\n", $5, $8 }'\'''
+  [kb-id]='get_xinput_id'
+  [kb-enable]='enable_laptop_keyboard'
+  [kb-disable]='disable_laptop_keyboard'
 )
+
+function get_xinput_id() {
+  xinput list | grep $1 | grep -oP '(?<=id=)\d+'
+}
+
+function enable_laptop_keyboard() {
+  local keyboard_id=$(get_xinput_id 'Virtual core keyboard')
+  xinput reattach $(get_xinput_id 'AT Translated Set 2 keyboard') $keyboard_id
+
+  local mouse_id=$(get_xinput_id 'Virtual core pointer')
+  xinput reattach $(get_xinput_id TrackPoint) $mouse_id
+}
+
+function disable_laptop_keyboard() {
+  xinput float $(get_xinput_id TrackPoint)
+  xinput float $(get_xinput_id 'AT Translated Set 2 keyboard')
+}
 
 # If for some reason I'm not using my keyboard, this makes the most important
 # bindings available to the standard keyboard using xmodmap.
