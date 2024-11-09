2fabd2c59349627a0f9b859b7ace19e799cf6b5d
Angelo Gazzola <angelo@nglgzz.com>
2024-11-09 21:45:55 +0100

    Update autoinstall configuration

---
+++ ubuntu/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -ex
+
+yq -n 'load("autoinstall.yml") * load("autoinstall.home.yml")' >./build/autoinstall.home.yml
+yq -n 'load("autoinstall.yml") * load("autoinstall.work.yml")' >./build/autoinstall.work.yml
