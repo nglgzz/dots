a6fed8f615f0039e1459ba339fbbd685c3f336a6
Angelo Gazzola <angelo@nglgzz.com>
2025-06-05 05:47:50 +0200

    Add function to parse usbmon output

--- dots/.config/zsh/aliases/shell.sh
+++ dots/.config/zsh/aliases/shell.sh
@@ -59,3 +59,75 @@ function eumount() {
   sudo umount "$2"
   sudo cryptsetup close "crypt_$1"
 }
+
+
+function mon-pretty() {
+    # Currently ignores number of frame descriptors for
+    # isochronous transfers.
+    #
+    # https://docs.kernel.org/usb/usbmon.html
+    cat "$1" | jq -nR '[
+      inputs |
+      split(" ") |
+      {
+        "urb_id": .[0],
+        "timestamp": .[1],
+        "event_type": .[2],
+        "address": .[3],
+        "status": .[4],
+        "rest": .[5:]
+      }
+    ]' | jq 'map(. | if .event_type == "S" then
+      .event_type = "Submission"
+    else
+      if .event_type == "C" then
+        .event_type = "Callback"
+      else
+        if .event_type == "E" then
+          .event_type = "Submission error"
+        end
+      end
+    end)' | jq 'map(. | if .status == "s" then
+      .setup_packet = .rest[0:5] |
+      .rest = .rest[5:] |
+      .status = "setup"
+    end)' | jq 'map(. | if .setup_packet then
+      .setup_packet = {
+        bmRequestType: .setup_packet[0],
+        bRequest: .setup_packet[1],
+        wValue: .setup_packet[2],
+        wIndex: .setup_packet[3],
+        wLength: .setup_packet[4],
+      }
+    end)' | jq 'map(. |
+      .data_length = .rest[0] |
+      .data_present = if .rest[1] == "=" then true else false end |
+      if (.rest | length) > 2 then
+        .data = .rest[2:]
+      end |
+      del(.rest)
+    )'
+}
+
+function mon-control() {
+  mon-pretty "$1" | jq 'map(select(.address | startswith("Ci")))'
+}
+
+# Captures USB traffic from the specified bus into the provided
+# output file. More information here on enabling the usbmon module
+#
+# https://docs.kernel.org/usb/usbmon.html
+#
+# Requires disabling secure boot to be able to disable the
+# kernel_lockdown preventing from logging traces from usbmon. More
+# info here:
+#
+# man kernel_lockdown.7
+#
+# Example usage:
+#   mon-capture 3 bus3.mon
+function mon-capture() {
+  sudo cat "/sys/kernel/debug/usb/usbmon/$1u" > ${2:-$1.mon}
+  sudo chown ${USER} ${2:-$1.mon}
+}
+
