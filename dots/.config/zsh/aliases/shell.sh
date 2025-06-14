11c3ee375a114441f60aaf873cc06137b9deace4
Angelo Gazzola <angelo@nglgzz.com>
2025-06-14 12:56:14 +0200

    Update usbmon utils

--- dots/.config/zsh/aliases/shell.sh
+++ dots/.config/zsh/aliases/shell.sh
@@ -31,8 +31,8 @@ declare -A shell=(
   [copy]='xclip -selection clipboard'
   [paste]='xclip -out -selection clipboard'
   [serve]='python3 -m http.server'
-  [free-port]=free_port
-  [random-word]='sort -R /usr/share/dict/cracklib-small | head -1'
+  [freeport]=free_port
+  [randomword]='sort -R /usr/share/dict/cracklib-small | head -1'
 )
 
 function free_port() {
@@ -60,13 +60,12 @@ function eumount() {
   sudo cryptsetup close "crypt_$1"
 }
 
-
 function mon-pretty() {
-    # Currently ignores number of frame descriptors for
-    # isochronous transfers.
-    #
-    # https://docs.kernel.org/usb/usbmon.html
-    cat "$1" | jq -nR '[
+  # Currently ignores number of frame descriptors for
+  # isochronous transfers.
+  #
+  # https://docs.kernel.org/usb/usbmon.html
+  cat "$1" | jq -nR '[
       inputs |
       split(" ") |
       {
@@ -127,7 +126,92 @@ function mon-control() {
 # Example usage:
 #   mon-capture 3 bus3.mon
 function mon-capture() {
-  sudo cat "/sys/kernel/debug/usb/usbmon/$1u" > ${2:-$1.mon}
-  sudo chown ${USER} ${2:-$1.mon}
+  sudo cat "/sys/kernel/debug/usb/usbmon/$1u" >"${2:-$1.mon}" || echo "Done capturing"
+  sudo chown "${USER}" "${2:-$1.mon}"
 }
 
+function mon-log() {
+  # Currently ignores number of frame descriptors for
+  # isochronous transfers.
+  #
+  # https://docs.kernel.org/usb/usbmon.html
+
+  sudo cat "/sys/kernel/debug/usb/usbmon/$1u" | jq -nR '
+      inputs |
+      split(" ") |
+      {
+        "urb_id": .[0],
+        "timestamp": .[1],
+        "event_type": .[2],
+        "address": .[3],
+        "status": .[4],
+        "rest": .[5:]
+      }
+    ' | jq -S 'if .event_type == "S" then
+      .event_type = "Submission"
+    else
+      if .event_type == "C" then
+        .event_type = "Callback"
+      else
+        if .event_type == "E" then
+          .event_type = "Submission error"
+        end
+      end
+    end' | jq 'if .status == "s" then
+      .setup_packet = .rest[0:5] |
+      .rest = .rest[5:] |
+      .status = "setup"
+    end' | jq ' if .setup_packet then
+      .setup_packet = {
+        bmRequestType: .setup_packet[0],
+        bRequest: .setup_packet[1],
+        wValue: .setup_packet[2],
+        wIndex: .setup_packet[3],
+        wLength: .setup_packet[4],
+      }
+    end' | jq '
+      .data_length = .rest[0] |
+      .data_present = if .rest[1] == "=" then true else false end |
+      if (.rest | length) > 2 then
+        .data = .rest[2:]
+      end |
+      del(.rest)
+    ' | jq 'if .status == "-75" then
+        .status = "-75 (-EOVERFLOW)"
+      else
+        if .status == "-32" then
+          .status = "-32 (-EPIPE)"
+        else
+          if .status == "-71" then
+            .status = "-71 (-EPROTO)"
+          else
+            if .status == "-110" then
+              .status = "-110 (-ETIMEDOUT)"
+            else
+              if .status == "-2" then
+                .status = "-2 (-ENOENT)"
+              end
+            end
+          end
+        end
+      end
+    '
+    # To find out more error codes:
+    #   cat /usr/include/asm-generic/errno.h
+}
+
+# Usage:
+#   mon-int bus address (or start of address)
+mon-int() {
+  mon-log "$1" | jq 'select(.address | contains("Ii:'"$1"':'"$2"'"))'
+}
+
+# Usage:
+#   mon-dev bus address (or start of address)
+mon-dev() {
+  mon-log "$1" | jq 'select(.address | contains(":'"$1"':'"$2"'"))'
+}
+
+dmesg-usb() {
+  sudo dmesg --notime -wd | grep --color=always usb
+}
