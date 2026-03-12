0551c9df0987e38d07625968d8f0f9140a6825ed
Angelo Gazzola <angelo@nglgzz.com>
2026-03-12 09:35:49 +0100

    add scripts to install extra packages

---
+++ fedora/extras/picoscope/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eu
+
+current_dir=$(dirname "$0")
+
+sudo rpmkeys --import http://labs.picotech.com/repomd.xml.key
+sudo cp "$current_dir"/picoscope.repo /etc/yum.repos.d/
+sudo dnf --assumeyes install picoscope
+
+# Workaround for segmentation fault when starting the app. Also updates the icon
+# because the default one is in .xpm format which seems to be deprecated (and
+# doesn't show up in the menus).
+#
+# https://www.picotech.com/support/viewtopic.php?p=152263&sid=b8122b09fc5a8fee834e32deea5fad16#p152263
+sudo cp "$current_dir"/ApplicationIcon.png /opt/picoscope/share/
+sudo desktop-file-edit /usr/share/applications/picotech-picoscope.desktop \
+    --set-key Exec --set-value 'env GLIBC_TUNABLES=glibc.rtld.execstack=2 /usr/bin/picoscope %U' \
+    --set-icon /opt/picoscope/share/ApplicationIcon.png
---
+++ fedora/extras/vscode/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+current_dir=$(dirname "$0")
+
+sudo rpmkeys --import https://packages.microsoft.com/keys/microsoft.asc
+sudo cp "$current_dir"/vscode.repo /etc/yum.repos.d/
+sudo dnf --assumeyes install code
