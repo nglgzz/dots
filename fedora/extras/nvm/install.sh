df7b0ff31dc64837acf3f9057d54119bec96df70
Angelo Gazzola <angelo@nglgzz.com>
2026-04-05 13:39:38 +0200

    remove gh dependency on nvm install script

--- fedora/extras/nvm/install.sh
+++ fedora/extras/nvm/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -ex
 
-latest_version=$(gh release view \
-	--repo nvm-sh/nvm \
-	--json 'tagName' --jq '.tagName')
+latest_version=$(
+	curl https://github.com/nvm-sh/nvm/releases/latest --silent --show-headers |
+		grep location |
+		sed 's|.*/||' |
+		tr -d '\r\n'
+)
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export PROFILE=/dev/null
